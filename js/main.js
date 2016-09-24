05327fd2468ba60dc782e2be7b7504d8432683bf
HaiyiMa <haiyima1987@gmail.com>
2016-09-24 14:23:10 +0200

    carousel and buttons finished. however, could be improved in the future.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,123 @@
 $(document).ready(function () {
 
+    // task 1: show the menu overlay
+    // variables for task 1
     var $bgMenu = $(".btnMenu");
     var $btnClose = $(".btnClose");
     var $overlay = $(".overlay");
     var $body = $("body");
-    var $delayTime = 200;
-    var $slidingTime = 500;
+    var $delayTimeMenu = 200;
+    var $showTimeMenu = 500;
 
+    // add event handlers
+    $bgMenu.click(showMenu);
+    $btnClose.click(closeMenu);
+
+    // show menu
     function showMenu() {
-        $overlay.delay($delayTime).slideDown($slidingTime);
+        $overlay.delay($delayTimeMenu).slideDown($showTimeMenu);
         $body.addClass("noScroll");
     }
 
+    // hide menu
     function closeMenu() {
-        $overlay.delay($delayTime).slideUp($slidingTime);
+        $overlay.delay($delayTimeMenu).slideUp($showTimeMenu);
         $body.removeClass("noScroll");
     }
 
-    $bgMenu.click(showMenu);
-    $btnClose.click(closeMenu);
+    // task 2: run the carousel
+    // variables for task 2
+    var carouselInterval;
+    var $pageIndex = 1;
+    var $pauseTimeCarousel = 5000;
+    var $slidingTimeCarousel = 1000;
+    var $width = '100%';
+    var $carousel = $(".carousel");
+    var $slides = $carousel.find(".slide");
+
+    // startSlider();
+
+    // start the slider
+    function startSlider() {
+        carouselInterval = setInterval(flipPage, $pauseTimeCarousel);
+    }
+
+    // flip one page
+    function flipPage() {
+        $carousel.animate({'margin-left': '-=' + $width}, $slidingTimeCarousel, pageRenew);
+    }
+
+    // renew page number
+    function pageRenew() {
+        $pageIndex++;
+        if ($pageIndex === $slides.length) {
+            $carousel.css('margin-left', 0);
+            $pageIndex = 1;
+        }
+    }
+
+    // task 3: click buttons to control the carousel
+    // variables for task 3
+    var $prev = $("#prev");
+    var $next = $("#next");
+    var $start = $("#start");
+    var $pause = $("#pause");
+    var $sliding = false;
+    var $delayTimeBtns = 100;
+    var $showTimeBtns = 100;
+
+    // add event handlers
+    $start.add($pause).click(toggleVisibility);
+    $start.on('click', startSlider);
+    $pause.on('click', stopSlider);
+    $prev.click(flipPageBack);
+    $next.click(flipPage);
+
+    // toggle start and stop button visibility
+    function toggleVisibility() {
+        $sliding = !$sliding;
+        if ($sliding) {
+            $pause.delay($delayTimeBtns).show($showTimeBtns);
+            $start.delay($delayTimeBtns).hide();
+        }
+        else {
+            $start.delay($delayTimeBtns).show($showTimeBtns);
+            $pause.delay($delayTimeBtns).hide();
+        }
+    }
+
+    // stop the slider
+    function stopSlider() {
+        clearInterval(carouselInterval);
+    }
+
+    // flip back one page for carousel buttons below
+    function flipPageBack() {
+        if ($pageIndex === 1) {
+            $pageIndex = $slides.length;
+            $lastPageMargin = -100 * ($slides.length - 1) + '%';
+            $carousel.css('margin-left', $lastPageMargin);
+        }
+        $carousel.animate({'margin-left': '+=' + $width}, $slidingTimeCarousel, pageRenewBack);
+    }
+
+    // flip one page back
+    function pageRenewBack() {
+        $pageIndex--;
+    }
+
+    // task 4: click the dots to control the carousel
+    // variables for task 4
+    var $dots = $(".pageDot");
+
+    // add event handlers
+    $dots.on("click", slideToSpecificPage);
+
+    // go to a specific page
+    function slideToSpecificPage() {
+        var $index = $dots.index($(this));
+        var $marginLeft = -100 * $index + "%";
+        $carousel.animate({'margin-left': $marginLeft}, $slidingTimeCarousel);
+        $pageIndex = $index + 1;
+    }
 })
