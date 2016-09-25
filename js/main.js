a079bd9611903eaddc9db919453968ae3631d07e
AdrianJRG <adrian.guillot@gmail.com>
2016-09-25 18:12:58 +0200

    please please please merge

--- js/main.js
+++ js/main.js
@@ -120,4 +120,44 @@ $(document).ready(function () {
         $carousel.animate({'margin-left': $marginLeft}, $slidingTimeCarousel);
         $pageIndex = $index + 1;
     }
+
+    // scroll back to top of page
+    jQuery(document).ready(function() {
+        var offset = 250;
+        var duration = 300;
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.to-top').fadeIn(duration);
+            } else {
+                jQuery('.to-top').fadeOut(duration);
+            }
+        });
+        jQuery('.to-top').click(function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
+    });
+
+    // hover over and select tables
+    $(".tableImage").hover(function() {
+        if ($(this).is(".selectedTable") === false){
+            $(this).attr('src', 'img/table_selected.jpg');
+        }
+    }, function() {
+        if ($(this).is(".selectedTable") === false) {
+            $(this).attr('src', 'img/table.jpg');
+        }
+    });
+
+    $(".tableImage").one("click", selectSeats);
+    
+    function selectSeats() {
+        $(this).addClass("selectedTable");
+        $(this).one("click", deselectSeats);
+    }
+
+    function deselectSeats() {
+        $(this).removeClass("selectedTable");
+        $(this).one("click", selectSeats);
 })
